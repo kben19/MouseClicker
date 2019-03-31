b7ad17f091cea874c9668392e80475a5cec2e772
kben19 <kben19@student.monash.edu>
2019-03-31 22:31:45 +1100

    initialize Model View

--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,19 @@
 /**
  * Created by benzali on 3/31/2019.
  */
+import java.awt.Robot;
+import java.util.Random;
+
 public class Model {
+
+    private Robot myRobot;
+
+    public Model() throws Exception{
+        myRobot = new Robot();
+    }
+
+    public void addAction(int x, int y){
+        myRobot.mouseMove(x, y);
+    }
+
 }
--- src/View.java
+++ src/View.java
@@ -1,5 +1,29 @@
 /**
  * Created by benzali on 3/31/2019.
  */
+import javax.swing.*;
+import java.awt.*;
+
 public class View {
+    private TextField xTextField, yTextField;
+    private Button submitButton;
+
+    public View(){
+        Frame frame = new Frame("Mouse Clicker");
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new FlowLayout());
+
+        xTextField = new TextField();
+        yTextField = new TextField();
+
+        submitButton = new Button("Submit");
+
+        panel1.add(new Label("X:"));
+        panel1.add(xTextField);
+        panel1.add(new Label("Y:"));
+        panel1.add(yTextField);
+        panel1.add(submitButton);
+
+    }
+
 }
